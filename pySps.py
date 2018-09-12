ca339b4eac28498c119029e3ebc2717ba42e32e2
zpvan <843762968@qq.com>
2018-09-12 15:19:01 +0800

    import bitstring module

--- pySps.py
+++ pySps.py
@@ -1,15 +1,20 @@
+# https://pythonhosted.org/bitstring/creation.html
+# pip3.6.exe install bitstring // for windows
+from bitstring import BitStream, BitArray
 
 class Sps():
 
     def __init__(self, rbsp):
         self.rbsp = rbsp
+        print("sps hex = " + self.rbsp.hex())
+        
 
     def parse(self):
         print("sps parse begin")
-        print("sps hex = " + self.rbsp.hex())
-
-
-
+        
+        str_sps_bin = BitArray(self.rbsp).bin
+        
+        print("sps binary = " + str_sps_bin)
 
         print("sps parse end")
 
